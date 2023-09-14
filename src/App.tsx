9ee5c54dc7718d5fefe6663077eb976e2f468362
pizzalawl <pizzalawls@gmail.com>
2023-09-13 21:34:36 -0500

    Main page UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
 import './App.css'
 
+interface TitleProps {
+  name: string;
+}
+
+interface ButtonProps {
+  label: string;
+}
+
+function Title(props: TitleProps) {
+  return (
+    <h1 className="text-7xl font-bold text-center relative top-5">{props.name}</h1>
+  );
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <button className='text-2xl bg-emerald-300 hover:bg-emerald-400 px-2 py-2 rounded'>{props.label}</button>
+  );
+}
+
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello World!</h1>
+      <Title name="itempile"/>
+      <div className='relative top-5'>
+        <div className='text-center relative top-10'><Button label="Games🎮"/></div>
+        <div className='text-center relative top-14'><Button label="Shop🪙"/></div>
+        <div className='text-center relative top-20'><Button label="Leaderboard🏆"/></div>
+      </div>
     </>
   )
 }
